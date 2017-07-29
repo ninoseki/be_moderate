f2f88038ee1d7691f4ef0196737d469080654fc6
ninoseki <zgok0079@gmail.com>
2017-07-30 02:25:02 +0900

    Update  cron job

--- app/cron.rb
+++ app/cron.rb
@@ -2,14 +2,17 @@ require_relative "../lib/miopon"
 
 include Miopon::Utils
 
-with_error_notify do
+def job
   monitor = Miopon::Monitor.new
   # notify
   monitor.over_limit_lines.each do |line|
     monitor.notify line
+    monitor.mark_as_notified line.code
   end
   # stats
   monitor.stats.each do |s|
     puts s
   end
 end
+
+with_error_notify { job }

554216efc0a48f9479f99bf4e12e2f64f646d801
ninoseki <zgok0079@gmail.com>
2017-09-11 19:38:49 +0900

    add a script for renewing a token

---
+++ app/renew_token.rb
@@ -0,0 +1,14 @@
+require 'uri'
+AUTHORIZATION = "https://api.iijmio.jp/mobile/d/v1/authorization/"
+
+params = {}
+params["response_type"] = "token"
+params["client_id"] = ENV["IIJMIO_DEVELOPER_ID"]
+params["redirect_uri"] = "https://be-moderate.herokuapp.com/"
+params["state"] = "hoge"
+
+puts [
+  AUTHORIZATION,
+  "?",
+  URI.encode_www_form(params)
+].join

12345e9493d2847371d7733ca2ae3b01d2e7762d
ninoseki <zgok0079@gmail.com>
2017-12-12 19:57:55 +0900

    remove a blank

--- app/renew_token.rb
+++ app/renew_token.rb
@@ -7,4 +7,4 @@ params["client_id"] = ENV["IIJMIO_DEVELOPER_ID"]
 params["redirect_uri"] = "https://be-moderate.herokuapp.com/"
 params["state"] = "hoge"
 
-puts [ AUTHORIZATION, "?", URI.encode_www_form(params)].join
+puts [AUTHORIZATION, "?", URI.encode_www_form(params)].join
